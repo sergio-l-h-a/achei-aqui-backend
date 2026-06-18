e35012f77c0b0148b0449ab4ed02651b3c13d032
Francisco-Sergio <lanhousearte@outlook.com>
2026-06-18 17:14:44 -0300

    Atualizando imagens

--- src/app.ts
+++ src/app.ts
@@ -5,11 +5,15 @@ import path from "node:path";
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: "https://achei-aqui-frontend.vercel.app",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+}));
 app.use(express.json());
 app.use(routes);
 
 // permite acessar a pasta de imagens
-app.use("/imagens", express.static(path.join(__dirname, "..", "imagens")));
+app.use("/imagens", express.static(path.join(__dirname, "imagens")));
 
 export default app;
