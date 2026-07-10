a258b110ff1c35d39237255e1e039fd56e5fc474
Francisco-Sergio <lanhousearte@outlook.com>
2026-07-10 19:42:51 -0300

    fix: atualizando pasta imagens no Express 3v

--- src/app.ts
+++ src/app.ts
@@ -17,6 +17,6 @@ app.use(express.json());
 app.use(routes);
 
 // permite acessar a pasta de imagens
-app.use("/imagens", express.static("imagens"));
+app.use("/imagens", express.static(path.join(__dirname, "imagens")));
 
 export default app;
