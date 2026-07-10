f021ac16828db83251beb753944f989592bd31b8
Francisco-Sergio <lanhousearte@outlook.com>
2026-07-10 20:20:49 -0300

    fix: Atualizando porta do servidor

--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,7 @@
 
 import app from "./app";
 
-const PORT = process.env.PORT || 3333;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
